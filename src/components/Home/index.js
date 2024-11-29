b09381e0bcd5fb6d74dbd523dac086400769cd06
Dartsac <idobson.gatech@gmail.com>
2024-11-28 22:10:12 -0500

    add text and style elements in home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,20 @@
+import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-h.png';
+import './index.scss'
 
 const Home = () => {
 
   return(
-  <div className="contianer home-page">
+  <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br /> I'm
+        <h1>Hi, <br /> I'm 
         <img src = {LogoTitle} alt = "writter"/>
-        halee
+        alee
         <br />
         writter
         </h1>
+        <h2>Crafting Words That Convert</h2>
+        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
       </div>
     </div>
   )
