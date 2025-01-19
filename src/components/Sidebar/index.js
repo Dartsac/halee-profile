c0608bea098897905caf381e935b23966d4c3f74
Dartsac <idobson.gatech@gmail.com>
2025-01-19 17:57:14 -0500

    update side bar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import './index.scss'
 import LogoH from '../../assets/images/logo-h.png'
 import LogoSubtitle from '../../assets/images/logo_sub_halee.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcase, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
@@ -13,9 +13,11 @@ const Sidebar = () => (
       <img className='sub-logo' src={LogoSubtitle} alt='halee' />
     </Link>
     <nav>
+
       <NavLink exact='true' activeclassname='active' to='/'>
         <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
       </NavLink>
+
       <NavLink
         exact='true'
         activeclassname='active'
@@ -24,6 +26,16 @@ const Sidebar = () => (
       >
         <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
       </NavLink>
+
+      <NavLink
+        exact='true'
+        activeclassname='active'
+        className='projects-link'
+        to='/projects'
+      >
+        <FontAwesomeIcon icon={faBriefcase} color='#4d4d4e' />
+      </NavLink>
+
       <NavLink
         exact='true'
         activeclassname='active'
@@ -32,6 +44,7 @@ const Sidebar = () => (
       >
         <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
       </NavLink>
+
     </nav>
     <ul>
       <li>
