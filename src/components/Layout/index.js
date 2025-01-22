c6091941d105cf41b701a77f5b4289de9163ebe8
Dartsac <idobson.gatech@gmail.com>
2025-01-22 14:39:41 -0500

    make only projects page as scrollable

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,30 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Sidebar from '../Sidebar'
 import './index.scss'
 
 const Layout = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname === '/projects') {
+      document.body.style.overflow = 'auto'
+    } else {
+      document.body.style.overflow = 'hidden'
+    }
+
+    return () => {}
+  }, [location.pathname])
+
+  const shouldForceOneScreen =
+    location.pathname === '/about' ||
+    location.pathname === '/contact' ||
+    location.pathname === '/'
+
   return (
     <div className='App'>
       <Sidebar />
-      <div className='page'>
+      <div className={`page ${shouldForceOneScreen ? 'fixed-height' : ''}`}>
         <span className='tags top-tags'>&lt;body&gt;</span>
 
         <Outlet />
