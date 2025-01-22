24f5f07ee2ae0a4bc165b2fd7b03b0ffc18b6262
Dartsac <idobson.gatech@gmail.com>
2025-01-22 12:40:26 -0500

    creating base projects page

---
+++ src/components/Projects/index.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Loader from '../Loader'
+import './index.scss'
+
+const Projects = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <>
+      <div className='container projects-page'>
+        <div className='text-zone'>
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+              idx={15}
+            />
+          </h1>
+        </div>
+      </div>
+      <Loader />
+    </>
+  )
+}
+
+export default Projects
