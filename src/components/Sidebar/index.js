188fec4b59dbf6e257e26db8929244550607f0b3
Dartsac <idobson.gatech@gmail.com>
2025-01-23 15:30:21 -0500

    update navigation
    for desktop I would like for the sidebar
    for mobile or smaller screens I would like a hamburger menu that slides
    down and takes up the screen

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoH from '../../assets/images/logo.png'
@@ -7,58 +8,102 @@ import {
   faEnvelope,
   faHome,
   faUser,
+  faBars, // add this for the hamburger icon
+  faTimes, // optional if you want an 'X' close icon
 } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
-const Sidebar = () => (
-  <div className='nav-bar'>
-    <Link className='logo' to='/'>
-      <img src={LogoH} alt='logo' />
-    </Link>
-    <nav>
-      <NavLink exact='true' activeclassname='active' to='/'>
-        <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
-      </NavLink>
+const Sidebar = () => {
+  // Track if the mobile nav is open
+  const [isOpen, setIsOpen] = useState(false)
 
-      <NavLink
-        exact='true'
-        activeclassname='active'
-        className='about-link'
-        to='/about'
-      >
-        <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
-      </NavLink>
+  // Toggle the nav
+  const toggleNav = () => setIsOpen(!isOpen)
 
-      <NavLink
-        exact='true'
-        activeclassname='active'
-        className='projects-link'
-        to='/projects'
-      >
-        <FontAwesomeIcon icon={faBriefcase} color='#4d4d4e' />
-      </NavLink>
+  return (
+    <>
+      {/* Hamburger button (only visible on small screens) */}
+      <button className='hamburger-btn' onClick={toggleNav}>
+        {isOpen ? (
+          <FontAwesomeIcon icon={faTimes} color='#fff' />
+        ) : (
+          <FontAwesomeIcon icon={faBars} color='#000' />
+        )}
+      </button>
 
-      <NavLink
-        exact='true'
-        activeclassname='active'
-        className='contact-link'
-        to='/contact'
-      >
-        <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a
-          target='_blank'
-          rel='noreferrer'
-          href='https://www.linkedin.com/in/halee-mccray-7040892a1/'
-        >
-          <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
-        </a>
-      </li>
-    </ul>
-  </div>
-)
+      {/* The sidebar */}
+      <div className={`nav-bar ${isOpen ? 'mobile-show' : ''}`}>
+        <Link className='logo' to='/' onClick={() => setIsOpen(false)}>
+          <img src={LogoH} alt='logo' />
+        </Link>
+        <nav>
+          <NavLink
+            exact='true'
+            activeclassname='active'
+            className='home-link'
+            to='/'
+            onClick={() => setIsOpen(false)} // close menu when link is clicked
+          >
+            <FontAwesomeIcon className='icon' icon={faHome} color='#4d4d4e' />
+            <span className='menu-text'>Home</span>
+          </NavLink>
+
+          <NavLink
+            exact='true'
+            activeclassname='active'
+            className='about-link'
+            to='/about'
+            onClick={() => setIsOpen(false)}
+          >
+            <FontAwesomeIcon className='icon' icon={faUser} color='#4d4d4e' />
+            <span className='menu-text'>About Me</span>
+          </NavLink>
+
+          <NavLink
+            exact='true'
+            activeclassname='active'
+            className='projects-link'
+            to='/projects'
+            onClick={() => setIsOpen(false)}
+          >
+            <FontAwesomeIcon
+              className='icon'
+              icon={faBriefcase}
+              color='#4d4d4e'
+            />
+            <span className='menu-text'>Projects</span>
+          </NavLink>
+
+          <NavLink
+            exact='true'
+            activeclassname='active'
+            className='contact-link'
+            to='/contact'
+            onClick={() => setIsOpen(false)}
+          >
+            <FontAwesomeIcon
+              className='icon'
+              icon={faEnvelope}
+              color='#4d4d4e'
+            />
+            <span className='menu-text'>Contact Me</span>
+          </NavLink>
+        </nav>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              rel='noreferrer'
+              href='https://www.linkedin.com/in/halee-mccray-7040892a1/'
+              onClick={() => setIsOpen(false)}
+            >
+              <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
 
 export default Sidebar
